77ce1ee18e69240901a0e9fcd5347475084b4f10
Ben Ning <zhesi.ning@ucdconnect.ie>
2019-10-11 15:27:09 +0100

    backup

--- src/components/AppHeader.vue
+++ src/components/AppHeader.vue
@@ -1,11 +1,15 @@
 <template>
-    <div>
-    Image Storage
+  <div class="ui secondary pointing menu">
+    <a class="item active">Image Storage</a>
+
+    <div class="right menu">
+      <a class="ui item">Logout</a>
     </div>
+  </div>
 </template>
 
 <script>
 export default {
-    name: 'AppHeader'
-}
+  name: "AppHeader"
+};
 </script>
